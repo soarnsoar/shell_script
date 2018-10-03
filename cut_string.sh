1955cbd7d29bae486e41336ba66b38b3e092fa2e
your name your last name <soarnsoar@gmail.com>
2018-10-03 23:32:47 +0900

    add more in cut_string

--- cut_string.sh
+++ cut_string.sh
@@ -1,4 +1,10 @@
 
-FilneName="job_10.log"
-jobnumber=${FileName%.log} ## %->remove part .log. *(asterisk) also 
+FileName="job_10.log"
+jobname=${FileName%.log} ## %->remove part .log. *(asterisk) also 
+
+echo "jobname="$jobname # job_10
+
+jobnumber=${jobname#job_}
 echo "jobnumber="$jobnumber
+
+
