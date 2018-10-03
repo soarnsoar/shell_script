605983357fed48d9ef15baf333e0e43182787866
your name your last name <soarnsoar@gmail.com>
2018-10-03 23:53:11 +0900

    add find_failed_job.sh

---
+++ find_failed_job.sh
@@ -0,0 +1,30 @@
+ #Error messages : Fatal Error Message, I stop this Run
+ARR_LogFileName=($(ls *.log))
+mkdir -p failed_jobs
+
+####Check whether a logfile has err msg####
+for LogFileName in ${ARR_LogFileName[@]};do
+
+    jobname=${LogFileName%.log}
+    errname=${jobname}.err
+    jobnumber=${jobname#job_}
+#echo "jobname="$jobname
+#FileName=job_1.log
+    echo $LogFileName   
+    echo "jobnumber="$jobnumber
+    ERR_MSG=`cat $LogFileName | grep "Fatal Error Message, I stop this Run"`   
+    echo "$ERR_MSG"
+    if [ -n "$ERR_MSG" ] 
+    then
+	echo "err"
+	mv OUTPUT_${jobnumber}.root failed_jobs/	
+    fi
+
+
+    ERR_MSG=`cat $errname | grep "Error detected in"`
+    if [ -n "${ERR_MSG}" ]  #Error detected in
+    then
+	echo "err"
+	mv OUTPUT_${jobnumber}.root failed_jobs/
+    fi
+done
