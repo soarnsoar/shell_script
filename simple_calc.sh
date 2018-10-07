08198d1663cfea76dacd42ae4893b734719506a4
jhchoi <soarnsoar@gmail.com>
2018-10-07 22:47:13 +0200

    Update simple_calc.sh

--- simple_calc.sh
+++ simple_calc.sh
@@ -1,4 +1,4 @@
- #!/bin/sh
+ #!/bin/sh                                                                                                                                     
 
 num1=20
 
@@ -8,8 +8,25 @@ plus=`expr $num1 + $num2`
 
 minus=`expr $num1 - $num2`
 
-mul=`expr $num1 \* $num2`    
+mul=`expr $num1 \* $num2`
 
 div=`expr $num1 / $num2`
 
 rem=`expr $num1 % $num2`
+
+
+echo $plus
+
+ARR1=( a b c )
+ARR2=( d e f )
+
+idx=0
+
+for i in ${ARR1[@]};do
+
+    for j in ${ARR2[@]};do
+        echo "idx="$idx
+        idx=`expr $idx + 1`
+    done
+
+done
