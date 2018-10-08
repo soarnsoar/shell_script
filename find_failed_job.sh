697e4e37d97a2574c42ad5e9b03f3fbc7c90ffbc
jhchoi <soarnsoar@gmail.com>
2018-10-08 16:25:17 +0200

    Update find_failed_job.sh

--- find_failed_job.sh
+++ find_failed_job.sh
@@ -2,7 +2,12 @@
 
 
 #Error messages : Fatal Error Message, I stop this Run
-ARR_LogFileName=($(ls *.log))
+
+ARR_ERR_PHR=("Fatal Error Message, I stop this Run" "Error detected in")
+#ARR_ERR_PHR[0]="Fatal Error Message, I stop this Run"
+#ARR_ERR_PHR[1]="Error detected in"
+#ARR_LogFileName=($(ls *.log))
+ARR_LogFileName=($(ls job_0.log))
 mkdir -p failed_jobs
 
 ####Check whether a logfile has err msg####
@@ -14,20 +19,27 @@ for LogFileName in ${ARR_LogFileName[@]};do
 #echo "jobname="$jobname
 #FileName=job_1.log
     echo $LogFileName   
-    echo "jobnumber="$jobnumber
-    ERR_MSG=`cat $LogFileName | grep "Fatal Error Message, I stop this Run"`   
-    echo "$ERR_MSG"
-    if [ -n "$ERR_MSG" ] 
-    then
-	echo "err"
-	mv OUTPUT_${jobnumber}.root failed_jobs/	
-    fi
+    #echo "jobnumber="$jobnumber
+  
 
-
-    ERR_MSG=`cat $errname | grep "Error detected in"`
-    if [ -n "${ERR_MSG}" ]  #Error detected in
-    then
-	echo "err"
-	mv OUTPUT_${jobnumber}.root failed_jobs/
-    fi
+    for err_phr_i in "${ARR_ERR_PHR[@]}";do
+	#echo $LogFileName
+	#echo $err_phr_i
+	ERR_MSG=`cat $LogFileName | grep "${err_phr_i}"`   
+	echo "$ERR_MSG"
+	if [ -n "$ERR_MSG" ] 
+	then
+	   # echo "err from .log"
+	    mv OUTPUT_${jobnumber}.root failed_jobs/ 
+	fi
+	
+	
+	ERR_MSG=`cat $errname | grep "${err_phr_i}"`
+        echo "$ERR_MSG"
+	if [ -n "${ERR_MSG}" ]  #Error detected in
+	then
+	  #  echo "err from .err"
+	    mv OUTPUT_${jobnumber}.root failed_jobs/
+	fi
+    done
 done
