a8a3ab6fd471f40b0c700c49d378a378d1287c27
your name your last name <soarnsoar@gmail.com>
2019-05-31 19:19:02 +0900

    add exception

--- check_nanoGardener_jobs.sh
+++ check_nanoGardener_jobs.sh
@@ -1,29 +1,34 @@
 #JOBDIR=NanoGardening__Autumn18_102X_nAODv4_GTv16_Full2018v4
-#JOBDIR=NanoGardening__Summer16_102X_nAODv4_Full2016v4
+JOBDIR=NanoGardening__Summer16_102X_nAODv4_Full2016v4
 #JOBDIR=NanoGardening__Run2016_102X_nAODv4_Full2016v4
-JOBDIR=NanoGardening__Fall2017_102X_nAODv4_Full2017v4
+#JOBDIR=NanoGardening__Fall2017_102X_nAODv4_Full2017v4
 #JOBDIR=NanoGardening__Summer16_102X_nAODv4_Full2016v4
 #JOBDIR=NanoGardening__Run2018_102X_nAODv4_14Dec_Full2018v4
 #JOBDIR=NanoGardening__Autumn18_102X_nAODv4_Full2018
 #JOBDIR=NanoGardening__Run2016_102X_nAODv4_Full2016v4
 #JOBDIR=NanoGardening__Run2017_102X_nAODv4_Full2017v4
-#TAG='Cor'
-TAG=''
+TAG='Cor'
+#TAG='rochester'
+VETOTAG=''
+VETOTAG='-v'
+
 ##-----Just check .done exists-----##
 pushd $JOBDIR
 echo "#Enter"
-ARR_ERRFILE=($(ls *.err|grep "$TAG"))
-ARR_TOTAL=($(ls *.sh | grep "$TAG"))
+ARR_ERRFILE=($(ls *.err|grep $VETOTAG "$TAG"))
+ARR_TOTAL=($(ls *.sh | grep $VETOTAG "$TAG"))
 NSUCCESS=0
 NFAIL=0
+NRUNNING=0
 NSUBMIT=0
 NTOTAL=${#ARR_TOTAL[@]}
 FAIL_LIST=()
 SUCCESS_LIST=()
-
+RUNNING_LIST=()
 for errfile in ${ARR_ERRFILE[@]};do
     NSUBMIT=`expr $NSUBMIT + 1`
     jobname=${errfile%".err"}
+    logfile=${jobname}.log
     echo "@jobname="$jobname
     echo "@@Check shell@@"
     #ls $jobname.sh
@@ -46,11 +51,17 @@ for errfile in ${ARR_ERRFILE[@]};do
 	#NSUCCESS=`expr $NSUCCESS + 1`
     #fi
     TRANSFER_ERR=`cat $errfile|grep 'Server responded with an error'`
-    if [[ -e "$jobname".done ]] && [[ -z "$TRANSFER_ERR" ]];then
+    GET_NANOAOD_ERR=`cat $errfile|grep 'attempt to access a null-pointer'`
+    TERMINATED_JOB=`cat $logfile|grep 'Job terminated'`
+    if [[ -e "$jobname".done ]] && [[ -z "$TRANSFER_ERR" ]] && [[ -z "$GET_NANOAOD_ERR" ]] && [[ -n "$TERMINATED_JOB" ]];then
 
 	SUCCESS_LIST+=($jobname)
         NSUCCESS=`expr $NSUCCESS + 1`
-
+	
+    elif [[ -z "$TERMINATED_JOB" ]];then
+	echo "-->Still Running"
+	echo "$TERMINATED_JOB"
+	NRUNNING=`expr $NRUNNING + 1`
     else
 	FAIL_LIST+=($jobname)                                                                                                                                
         NFAIL=`expr $NFAIL + 1`                                                                                                                              
@@ -60,6 +71,7 @@ for errfile in ${ARR_ERRFILE[@]};do
 done
 echo "NSUCCESS="$NSUCCESS
 echo "NFAIL="$NFAIL
+echo "NRUNNING="$NRUNNING
 echo "NSUBMIT="$NSUBMIT
 echo "NTOTAL="$NTOTAL
 popd
@@ -102,7 +114,7 @@ if [ $isresubmit -eq 1 ];then
 	mv $failjob.out fail_jobs/
 	mv $failjob.err fail_jobs/
 	mv $failjob.log fail_jobs/
-	condor_submit $failjob.jds
+	condor_submit $failjob.jds &> $failjob.jid
 
     done
 
